6dba34a3f927e25fef3c254a381e6523b95a397f
diegolarraz <diegolarraz1@gmail.com>
2020-09-20 15:52:14 +0100

    finished with app

--- src/index.js
+++ src/index.js
@@ -1,34 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
-
+import Spinner from './Spinner';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { lat: null, errorMessage: '' };
-
+    state = { lat: null, errorMessage: '' };
+    
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude});
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude}),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
 
-    render() {
+    renderContent () {
         if (this.state.errorMessage && !this.state.lat) {
             return  <div>Error: {this.state.errorMessage}</div>;
         } 
         
         if (!this.state.errorMessage && this.state.lat) {
-            return  <div>Latitude: {this.state.lat}</div>;
+            return  <div><SeasonDisplay lat={this.state.lat} /></div>;
         }
 
-        return <div>Loading...</div>;
+        return <Spinner text="Please Allow your Location!" />;
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
